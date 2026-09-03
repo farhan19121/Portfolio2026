6b068760bb8b69901fd3dca926f4031cce1dd66a
Farhan <farlockisplaying@gmail.com>
2026-09-03 21:25:38 +0530

    Updated project

--- backend/portfolio_backend/wsgi.py
+++ backend/portfolio_backend/wsgi.py
@@ -1,16 +1,14 @@
-"""
-WSGI config for portfolio_backend project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+from pathlib import Path
 
-from django.core.wsgi import get_wsgi_application
+# Add backend directory to sys.path so django apps can be resolved on Vercel
+BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.append(str(BASE_DIR))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_backend.settings')
 
+from django.core.wsgi import get_wsgi_application
+
 application = get_wsgi_application()
+app = application
